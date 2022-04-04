377fe6fb32bdcd9a7a1afa02777f5c0b454d98e8
Kelvin Nunez <knunezweb@gmail.com>
2022-04-04 13:15:15 -0400

    Added UUID

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke() {
+    return 'My dad is tougher than you'
+}
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+import generateJoke from "./generateJoke";
+
+
+console.log(uuidv4());
